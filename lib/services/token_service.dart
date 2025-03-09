c851c73d33b5062bdc61b4fa4f92b90da8b1c5df
Jannabel Ramos Ramírez <jannabelramos@hotmail.com>
2025-03-09 16:09:21 -0400

    fix: token lib

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -1,25 +1,34 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenService implements ITokenService {
   final storage = const FlutterSecureStorage();
 
   @override
   Future<Token?> getToken() async {
-    var accessToken = await storage.read(key: 'accessToken') ?? '';
-    var refreshToken = await storage.read(key: 'refreshToken') ?? '';
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getString('accessToken');
+    var accessToken = prefs.getString(
+        'accessToken'); //await storage.read(key: 'accessToken') ?? '';
+    var refreshToken = prefs.getString(
+        'refreshToken'); // await storage.read(key: 'refreshToken') ?? '';
 
     var tokens = Token(
-      accessToken: accessToken,
-      refreshToken: refreshToken,
+      accessToken: accessToken!,
+      refreshToken: refreshToken!,
     );
     return tokens;
   }
 
   @override
   Future<void> setToken(Token tokens) async {
-    await storage.write(key: 'accessToken', value: tokens.accessToken);
-    await storage.write(key: 'userId', value: tokens.userId.toString());
-    await storage.write(key: 'refreshToken', value: tokens.refreshToken);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('accessToken', tokens.accessToken);
+    await prefs.setString('refreshToken', tokens.refreshToken!);
+
+    // await storage.write(key: 'accessToken', value: tokens.accessToken);
+    // await storage.write(key: 'userId', value: tokens.userId.toString());
+    // await storage.write(key: 'refreshToken', value: tokens.refreshToken);
     // var expirationTime =
     //     DateTime.now().add(Duration(seconds: tokens.expireIn!));
 
